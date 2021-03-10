44fa4a29766bfa59e7a7a522b68a01dba9a05b09
Julie J <jeannet.julie@gmail.com>
2021-03-10 23:16:13 +0100

    Tri des données 30 pays les plus peuplés

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -5,20 +5,45 @@ import Card from './Card';
 const Countries = () => {
 
   const [data, setData] = useState([]);
-  // const [playOnce, setPlayOnce] = useState(true);
+  const [sortedData, setSortedData] = useState([]);
+  const [playOnce, setPlayOnce] = useState(true);
   useEffect(() => {
-    axios
-      .get(
-        "https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
-      )
-      .then((res) => setData(res.data))
-  }, []);
+if(playOnce){
+
+  axios
+    .get(
+      "https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag"
+    )
+    .then((res) => {
+      setData(res.data)
+      setPlayOnce(false);
+    }
+);
+  }
+
+
+      const sortedCountry=()=>{
+        const countryObj=Object.keys(data).map((i)=>data[i]);
+        const sortedArray=countryObj.sort((a,b)=>{
+          return b.population - a.population;
+        });
+        sortedArray.lenght=30;
+        setSortedData(sortedArray);
+
+      };
+      sortedCountry();
+
+  }, [data, playOnce]);
+
+  
+
+
 
   return (
     <div className="countries">
       <ul className="countries-list">
 
-        {data.map((country) =>
+        {sortedData.map((country) =>
           // on crée un props en envoyant les données de country à la card
           // sans oublier de passer une clée unique à chaque élément que l'on map
           <Card country={country} key={country.name} />
